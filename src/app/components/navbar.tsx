d959c192770e2952478d0173e8c086bbf2af7358
MelloTheCatLover <misha.kozoliy@gmail.com>
2024-10-23 16:03:28 +0700

    Add cool dashboard button

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -1,4 +1,6 @@
 import { div, header } from "framer-motion/client";
+import { MenuIcon } from "lucide-react";
+import Link from "next/link";
 import React from "react";
 
 type Props = {}
@@ -8,7 +10,35 @@ const Navbar = (props: Props) => {
   backdrop-blur-lg z-[100] flex items-center border-b-[1px]
   border-neutral-900 justify-between" > 
     <aside className="flex items-center gap-[2px]">
-      <p>Chat-Bot by NSU</p>
+      <p className="text-3xl font-bold">Chat-Bot by NSU</p>
+    </aside>
+    <nav className="absolute left-[50%] top-[50%] transform translate-x-[-50%] translate-y-[-50%] hidden md:block">
+      <ul className="flex items-center gap-4 list-none">
+        <li>
+          <Link href="#">About Us</Link>
+        </li>
+        <li>
+          <Link href="#">About Us</Link>
+        </li>
+        <li>
+          <Link href="#">About Us</Link>
+        </li>
+        <li>
+          <Link href="#">About Us</Link>
+        </li>
+      </ul>
+    </nav>
+    <aside className="flex items-center gap-4">
+      <Link
+        href="/bots"
+        className="relative inline-flex h-10 overflow-hidden rounded-full p-[2px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
+          <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]"/>
+          <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl">
+            {true ? 'Bot\'s' : 'Get Started'}
+          </span>
+      </Link>
+
+      <MenuIcon className="md:hidden"></MenuIcon>
     </aside>
   </header> 
 }
